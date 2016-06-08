4389b937e3a21a69e8d14acb5484ec8e351a6d45
Siddharth Verma <siddharthv93@gmail.com>
2016-06-08 21:42:45 +0530

    Delete, Add functions

--- textEditor.py
+++ textEditor.py
@@ -27,7 +27,7 @@ class TextEditor:
         return n > maxLines 
 
     def checkMultipleBounds(self, n, m):
-        if (self.checkLowerBound(n) or self.checkUpperBound(m, self.numLines)):
+        if (self.checkLowerBound(n) or self.checkUpperBound(m, self.numLines) or n > m):
             return TextEditor.invalidOperation
     
     def checkBounds(self, n):
@@ -57,14 +57,33 @@ class TextEditor:
         
         self.text.pop(n - 1)
         self.numLines -= 1
+
+    def deleteMultipleLines(self, n, m):
+        if self.checkMultipleBounds(n,m) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
+        
+        self.text = self.text[0 : n - 1] + self.text[m : self.numLines]
+        self.numLines -= m - n + 1
+
+    def copyMultipleLines(self, n, m):
+        if self.checkMultipleBounds(n,m) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
+        
+        self.clipBoard = self.text[n - 1 : m]
+
+    def pasteAtN(self, n):
+        if self.checkBounds(n) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
+        
+        self.text = self.text[0 : n] + self.clipBoard + self.text[n : self.numLines]
+        self.numLines = len(self.text)
+        
 string = "Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java. The language provides constructs intended to enable clear programs on both a small and large scale."
 
 textEditor = TextEditor()
 textEditor.wrapText(string)
 textEditor.displayText()
-print textEditor.displaySpecificLines(4,5)
-textEditor.insertLineAtN("Abc", 3)
-textEditor.displayText()
-textEditor.deleteLineAtN(3)
-textEditor.deleteLineAtN(3)
+textEditor.copyMultipleLines(4,7)
+print textEditor.clipBoard
+textEditor.pasteAtN(9)
 textEditor.displayText()
