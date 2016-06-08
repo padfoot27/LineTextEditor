2d899a7bdc10384f1372314e8b4b343e323bc7ea
Siddharth Verma <siddharthv93@gmail.com>
2016-06-08 21:15:01 +0530

    Create insert and delete at line functions

--- textEditor.py
+++ textEditor.py
@@ -26,20 +26,45 @@ class TextEditor:
     def checkUpperBound(self, n, maxLines):
         return n > maxLines 
 
+    def checkMultipleBounds(self, n, m):
+        if (self.checkLowerBound(n) or self.checkUpperBound(m, self.numLines)):
+            return TextEditor.invalidOperation
+    
+    def checkBounds(self, n):
+        if (self.checkLowerBound(n) or self.checkUpperBound(n, self.numLines)):
+            return TextEditor.invalidOperation
+
     def displayText(self):
         for i in xrange(self.numLines):
             print (str) (i + 1) + " " + self.text[i]
 
     def displaySpecificLines(self, n, m):
-        if (self.checkLowerBound(n) or self.checkUpperBound(m, self.numLines)):
-            return 'Invalid operation'
+        if self.checkMultipleBounds(n,m) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
 
         return self.text[n - 1 : m]
 
+    def insertLineAtN(self, insertText, n):
+        if self.checkBounds(n) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
+        
+        self.text.insert(n - 1, insertText)
+        self.numLines += 1
 
+    def deleteLineAtN(self, n):
+        if self.checkBounds(n) == TextEditor.invalidOperation:
+            return TextEditor.invalidOperation
+        
+        self.text.pop(n - 1)
+        self.numLines -= 1
 string = "Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java. The language provides constructs intended to enable clear programs on both a small and large scale."
 
 textEditor = TextEditor()
 textEditor.wrapText(string)
 textEditor.displayText()
 print textEditor.displaySpecificLines(4,5)
+textEditor.insertLineAtN("Abc", 3)
+textEditor.displayText()
+textEditor.deleteLineAtN(3)
+textEditor.deleteLineAtN(3)
+textEditor.displayText()
