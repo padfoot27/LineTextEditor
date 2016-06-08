0ee6b49f0b949f98bbd76f139cf9533b4cc3e01c
Siddharth Verma <siddharthv93@gmail.com>
2016-06-08 22:46:04 +0530

    Create parser for commands, add copy and paste

--- textEditor.py
+++ textEditor.py
@@ -1,6 +1,17 @@
 class TextEditor:
     
     invalidOperation = 'Invalid operation' 
+    
+    # Commands
+    dispayContentCommand = "d";
+    displaySpecificLinesCommand = "d."
+    insertLineCommand = "i";
+    deleteLineCommand = "dd";
+    copyLineCommand = "y";
+    copyLineCommandFull = "yy"
+    pasteCommand = "p";
+    undoCommand = "z";
+    redoCommand = "zz";
 
     def __init__(self):
         self.numLines = 0
@@ -42,7 +53,8 @@ class TextEditor:
         if self.checkMultipleBounds(n,m) == TextEditor.invalidOperation:
             return TextEditor.invalidOperation
 
-        return self.text[n - 1 : m]
+        for i in xrange(n - 1, m):
+            print self.text[i]
 
     def insertLineAtN(self, insertText, n):
         if self.checkBounds(n) == TextEditor.invalidOperation:
@@ -78,12 +90,125 @@ class TextEditor:
         self.text = self.text[0 : n] + self.clipBoard + self.text[n : self.numLines]
         self.numLines = len(self.text)
         
+    def startTextEditor(self):
+
+        while True:
+            command = raw_input()
+            lenCommand = len(command)
+
+            if lenCommand == 0:
+                print TextEditor.invalidOperation
+
+            elif lenCommand == 1 and command == TextEditor.dispayContentCommand:
+                self.displayText()
+
+            elif lenCommand == 1 and command == TextEditor.undoCommand:
+                pass
+            
+            elif lenCommand < 2:
+                print TextEditor.invalidOperation
+
+            elif command[0 : 2] == TextEditor.displaySpecificLinesCommand:
+                command = command.split(".")
+                n = -1
+                m = -1
+
+                try:
+                    n = int(command[1])
+                    m = int(command[2])
+                except:
+                    print TextEditor.invalidOperation
+
+                self.displaySpecificLines(n , m)
+
+            elif command[0] == TextEditor.insertLineCommand:
+                command = command.split(".")
+
+                n = -1
+
+                try:
+                    n = int(command[1])
+                except:
+                    print TextEditor.invalidOperation
+                    return
+
+                self.insertLineAtN(command[2], n)
+                
+            
+            elif command[0] == TextEditor.copyLineCommand:
+                
+                command = command.split(".")
+                if command[0] != TextEditor.copyLineCommandFull:
+                    print TextEditor.invalidOperation
+                    return
+                n = -1
+                m = -1
+                try :
+                    n = int(command[1])
+                    m = int(command[2])
+                except:
+                    print TextEditor.invalidOperation
+                    return
+
+                self.copyMultipleLines(n , m)
+
+            elif command[0] == TextEditor.pasteCommand:
+                
+                command = command.split(".")
+                
+                n = -1 
+                
+                try:
+                    n = int(command[1])
+
+                except:
+                    print TextEditor.invalidOperation
+
+                self.pasteAtN(n)
+
+            elif command == TextEditor.redoCommand:
+                pass
+
+            elif lenCommand < 3:
+                print TextEditor.invalidOperation
+
+            elif command[0 : 2] == TextEditor.deleteLineCommand and len(command.split(".")) == 2:
+                
+                command = command.split(".")
+
+                n = -1
+
+                try:
+                    n = int(command[1])
+                except:
+                    print TextEditor.invalidOperation
+                    return
+                
+                self.deleteLineAtN(n)
+
+            elif command[0 : 2] == TextEditor.deleteLineCommand and len(command.split(".")) == 3:
+                
+                command = command.split(".")
+
+                try:
+                    n = int(command[1])
+                    m = int(command[2])
+
+                except:
+                    print TextEditor.invalidOperation
+                    return
+
+                self.deleteMultipleLines(n, m)
+
+            else:
+                print TextEditor.invalidOperation
+
+
 string = "Python is a widely used high-level, general-purpose, interpreted, dynamic programming language. Its design philosophy emphasizes code readability, and its syntax allows programmers to express concepts in fewer lines of code than possible in languages such as C++ or Java. The language provides constructs intended to enable clear programs on both a small and large scale."
 
 textEditor = TextEditor()
 textEditor.wrapText(string)
-textEditor.displayText()
-textEditor.copyMultipleLines(4,7)
-print textEditor.clipBoard
-textEditor.pasteAtN(9)
-textEditor.displayText()
+textEditor.startTextEditor()
+
+
+
